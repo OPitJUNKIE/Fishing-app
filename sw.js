a2322cc8ef20780294a7fbe33f7876d63370ac8d
Fishinforanapp <patcastrechini@gmail.com>
2025-08-12 12:06:42 +0300

    Add PWA fishing app files

---
+++ sw.js
@@ -0,0 +1,20 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('bite-v1').then(cache => cache.addAll([
+      './index.html',
+      './manifest.webmanifest',
+      './bite_app_icon_512.png'
+    ]))
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+  } else {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  }
+});
