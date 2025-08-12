fd8a78367f80723ce80b8f6577397e801e4d1035
Fishinforanapp <patcastrechini@gmail.com>
2025-08-13 00:25:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,28 @@
-// sw.js
+// v4.3.1 cache (cache name bump ensures new index is served)
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('bite-v3').then(cache => cache.addAll([
-      './index.html',
-      './manifest.webmanifest',
-      './bite_app_icon_512.png'
+    caches.open('bite-v4.3.1').then(cache => cache.addAll([
+      'index.html',
+      'manifest.webmanifest',
+      'bite_app_icon_512.png'
     ]))
   );
   self.skipWaiting();
 });
-self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (url.origin === location.origin) {
-    event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+    event.respondWith(
+      caches.match(event.request).then(r => r || fetch(event.request))
+    );
   } else {
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(event.request))
+    );
   }
 });
